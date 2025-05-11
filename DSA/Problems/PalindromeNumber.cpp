3bfc60f11f9b9715754035c4315cf0b8a6a346c9
sachinkumar012 <sachinyadav887780@gmail.com>
2025-05-11 23:29:05 +0530

    palindrome

---
+++ DSA/Problems/PalindromeNumber.cpp
@@ -0,0 +1,35 @@
+#include<bits/StdC++.h>
+using namespace std;
+
+// int main(){
+//     int n;
+//     cin>>n;
+//     int rev=0;
+//     int dup=n;
+//     while(n!=0){
+//         int ld=n%10;
+//         rev=(rev*10)+ld;
+//         n/=10;
+//     }
+//     if(rev==dup) cout<<"Palindrome Number";
+//     else cout<<"Not a Palindrome Number";
+// }
+
+
+// Leetocde 
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x<0) return false;
+        int dup=x;
+        int rev=0;
+        while(x!=0){
+            long ld=x%10;
+            if (rev > (INT_MAX - ld) / 10) return false;
+            rev=(rev*10)+ld;
+
+            x=x/10;
+         }
+      return rev==dup;
+     }
+};
