60fff5c8690276141d516dc58ee2aad67ffd6e99
sachinkumar012 <sachinyadav887780@gmail.com>
2025-05-11 18:55:07 +0530

    comment add

--- DSA/Problems/ReverseNumber.cpp
+++ DSA/Problems/ReverseNumber.cpp
@@ -7,12 +7,12 @@ Ex: 1000=1, 1354=4531(revrse no)
 */
 int main(){
     int n;
-    cin>>n;
+    cin>>n;  // 4531
     int rev=0;
     while(n>0){
-        int lastNo=n%10;
-        rev=(rev*10)+lastNo;
-        n=n/10;
+        int lastNo=n%10;  //4531%10=1
+        rev=(rev*10)+lastNo;  // (0*10)+1=1
+        n=n/10;  
     }
     cout<<rev;
 }
