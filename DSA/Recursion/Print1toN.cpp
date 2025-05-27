82e2932bfc467bcaccd5a08ac87318a793706016
sachinkumar012 <sachinyadav887780@gmail.com>
2025-05-27 09:43:48 +0530

    revision

--- DSA/Recursion/Print1toN.cpp
+++ DSA/Recursion/Print1toN.cpp
@@ -1,9 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+// void printNumber(int n){
+//     if(n>10) return;
+//     cout<<n<<endl;
+//     printNumber(n+1);
+// }
+
+// int main(){
+//     printNumber(1);
+//     return 0;
+// }
+
+
 void printNumber(int n){
     if(n>10) return;
-    cout<<n<<endl;
+    cout<<n<<" ";
     printNumber(n+1);
 }
 
