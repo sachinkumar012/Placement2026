f5118fd9f4f2b881955c0817a0ff5af7b7743ef9
sachinkumar012 <sachinyadav887780@gmail.com>
2025-06-03 22:27:44 +0530

    hashing

--- DSA/Hashing/Hashing.cpp
+++ DSA/Hashing/Hashing.cpp
@@ -1,15 +1,34 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+/* Using Normal Technique */
+
+// int main(){
+//     int arr[]={1,2,3,4,2,1};
+//     int n=sizeof(arr)/sizeof(arr[0]);
+//     int count=0;
+//     int key=1;
+//     for(int i=0;i<n;i++){
+//         if(arr[i]==key){
+//             count++;
+//         }
+//     }
+//     cout<<"Occurinng of "<<key<<" in array is: "<<count;
+//  }
+
+
+/* Using Hashing (Unordered map) */
+
 int main(){
-    int arr[]={1,2,3,4,2,1};
+    int arr[]={1,2,3,1,2,3,4,2};
     int n=sizeof(arr)/sizeof(arr[0]);
-    int count=0;
-    int key=1;
+    int key;
+    cout<<"Enter key to find the occurance: ";
+    cin>>key;
+    unordered_map<int,int> freq;
     for(int i=0;i<n;i++){
-        if(arr[i]==key){
-            count++;
-        }
+        freq[arr[i]]++;
     }
-    cout<<"Occurinng of "<<key<<" in array is: "<<count;
- }
+    cout<<"frequency of "<<key<<" is: "<<freq[key];
+    return 0;
+}
