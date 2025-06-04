145dfd2d3cadd3e68ddd110afc00797ef5358635
sachinkumar012 <sachinyadav887780@gmail.com>
2025-06-04 22:12:11 +0530

    hashing

--- DSA/Hashing/Hashing.cpp
+++ DSA/Hashing/Hashing.cpp
@@ -19,16 +19,39 @@ using namespace std;
 
 /* Using Hashing (Unordered map) */
 
+// int main(){
+//     int arr[]={1,2,3,1,2,3,4,2};
+//     int n=sizeof(arr)/sizeof(arr[0]);
+//     int key;
+//     cout<<"Enter key to find the occurance: ";
+//     cin>>key;
+//     unordered_map<int,int> freq;
+//     for(int i=0;i<n;i++){
+//         freq[arr[i]]++;
+//     }
+//     cout<<"frequency of "<<key<<" is: "<<freq[key];
+//     return 0;
+// }
+
 int main(){
-    int arr[]={1,2,3,1,2,3,4,2};
-    int n=sizeof(arr)/sizeof(arr[0]);
-    int key;
-    cout<<"Enter key to find the occurance: ";
-    cin>>key;
-    unordered_map<int,int> freq;
+    int n;
+    cin>>n;
+    int arr[n];
     for(int i=0;i<n;i++){
-        freq[arr[i]]++;
+        cin>>arr[i];
     }
-    cout<<"frequency of "<<key<<" is: "<<freq[key];
+
+    int hash[12]={0};
+    for(int i=0;i<n;i++){
+        hash[arr[i]] +=1;
+    }
+
+    int q;
+    cin>>q;
+    while(q--){
+        int number;
+        cin>>number;
+        cout<<hash[number]<<endl;
+    }
     return 0;
 }
