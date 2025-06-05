0078c4fd13ebc2cf61e83cd7bb1b2c95eedd5b4a
sachinkumar012 <sachinyadav887780@gmail.com>
2025-06-05 23:41:54 +0530

    hello

--- DSA/Hashing/Hashing.cpp
+++ DSA/Hashing/Hashing.cpp
@@ -33,25 +33,56 @@ using namespace std;
 //     return 0;
 // }
 
-int main(){
-    int n;
-    cin>>n;
-    int arr[n];
-    for(int i=0;i<n;i++){
-        cin>>arr[i];
+// int main(){
+//     int n;
+//     cin>>n;
+//     int arr[n];
+//     for(int i=0;i<n;i++){
+//         cin>>arr[i];
+//     }
+
+//     int hash[12]={0};
+//     for(int i=0;i<n;i++){
+//         hash[arr[i]] +=1;
+//     }
+
+//     int q;
+//     cin>>q;
+//     while(q--){
+//         int number;
+//         cin>>number;
+//         cout<<hash[number]<<endl;
+//     }
+//     return 0;
+// }
+
+
+
+
+/*
+1. Range inside main function for integer : 10^6 / 1e6
+2. Range outside main funtion for integer : 10^7 / 1e7
+
+3. Range inside main function for boolean : 10^7 / 1e7
+4. Range outside main function for boolean : 10^8 / 1e8
+
+*/
+ 
+
+int main() {
+    string s = "hello world";
+    unordered_map<char, int> freq;
+
+    // Count character frequency using hashing
+    for (char c : s) {
+        if (c != ' ') // skip spaces
+            freq[c]++;
     }
 
-    int hash[12]={0};
-    for(int i=0;i<n;i++){
-        hash[arr[i]] +=1;
+    // Print frequencies
+    for (auto pair : freq) {
+        cout << pair.first << " => " << pair.second << endl;
     }
 
-    int q;
-    cin>>q;
-    while(q--){
-        int number;
-        cin>>number;
-        cout<<hash[number]<<endl;
-    }
     return 0;
 }
